b6fcd6a1795acc9d8953345fceaaa86060d2ff4c
Isaac Puch Rojo <puchrojo@gmail.com>
2020-02-26 08:03:55 +0100

    Get v0.0.1-2 and not v0.0.2

--- bin/all.sh
+++ bin/all.sh
@@ -2,9 +2,11 @@
 
 ./00-create-user.sh
 ./01-install-build-packages
-# ./02-cahnge-user
 ./03-start-04-as-badpeswg
 #./04-init-package
 ./05-install-dummy-hello_0.0.1-1_amd64.deb
-#./06-start-07
+./06-start-07
+#./07-update-package
 
+
+#./10-cahnge-user
